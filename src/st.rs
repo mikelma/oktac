e80b1cd7e5e83602e49c526b8268e539b8d43188
mikelma <mikelma7@gmail.com>
2021-07-30 16:16:06 +0200

    st method search_var added

--- src/st.rs
+++ src/st.rs
@@ -46,12 +46,44 @@ impl SymbolTableStack {
         }
     }
 
-    pub fn record_func(&mut self, name: &str, ret_val: Option<VarType>, params: Vec<(String, VarType)>) {
+    pub fn record_func(&mut self, name: &str, 
+                       ret_val: Option<VarType>, 
+                       params: Vec<(String, VarType)>) -> Result<(), LogMesg<&str>> {
         let table = self.stack.iter_mut().last().expect("Symbol table stack is empty");
         let params_map = HashMap::from_iter(params);
-        table.insert(name.to_string(), SymbolInfo::Function {
-            ret_ty: ret_val, 
-            params: params_map,
-        });
+        if let Some(SymbolInfo::Function {..}) = table.get(name) {
+            Err(LogMesg::err()
+                .name("Invalid name")
+                .cause("There is a function with the same name in the current scope")
+                .help("Consider renaming the function"))
+        } else {
+            table.insert(name.to_string(), SymbolInfo::Function {
+                ret_ty: ret_val, 
+                params: params_map,
+            });
+            Ok(())
+        }
+    }
+
+    fn search(&self, symbol: &str) -> Option<&SymbolInfo> {
+        if let Some(table) = self.stack.iter().rev().find(|t| t.contains_key(symbol)) {
+            return table.get(symbol);
+        }
+        None
+    }
+
+    pub fn search_var(&self, symbol: String) -> Result<&SymbolInfo, LogMesg<&str>> {
+        if let Some(info) = self.search(&symbol) {
+            match info {
+                SymbolInfo::Var(..) => Ok(info),
+                _ => Err(LogMesg::err()
+                .name("Variable not defined")
+                .cause(format!("{} is a function not a variable", symbol).as_ref()))
+            }
+        } else {
+            Err(LogMesg::err()
+                .name("Variable not defined")
+                .cause(format!("Variable {} was not declared in this scope", symbol).as_str()))
+        }
     }
 }
