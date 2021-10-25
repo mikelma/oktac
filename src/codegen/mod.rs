b55a82de533c7211fb01ef5daac605bc9a49b72d
mikelma <mikelma7@gmail.com>
2021-10-25 16:27:30 +0200

    utils module added to codegen

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,3 +1,5 @@
+mod utils;
+
 use inkwell::basic_block::BasicBlock;
 use inkwell::builder::Builder;
 use inkwell::context::Context;
@@ -15,7 +17,7 @@ use either::Either;
 use std::collections::HashMap;
 use std::fmt;
 
-use crate::{ast::*, VarType, ST};
+use crate::{ast::*, VarType};
 
 const FN_RET_BB: &str = "ret.bb";
 
@@ -55,30 +57,6 @@ impl<'ctx> CodeGen<'ctx> {
             curr_fn_ret_val: None,
             curr_fn_ret_bb: None,
             loop_exit_bb: None,
-        }
-    }
-
-    fn create_entry_block_alloca<T: BasicType<'ctx>>(
-        &self,
-        name: &str,
-        var_type: T,
-    ) -> PointerValue<'ctx> {
-        let builder = self.context.create_builder();
-        let entry = self.curr_func.unwrap().get_first_basic_block().unwrap();
-        match entry.get_first_instruction() {
-            Some(first_instr) => builder.position_before(&first_instr),
-            None => builder.position_at_end(entry),
-        }
-
-        builder.build_alloca(var_type, name)
-    }
-
-    fn create_basic_block(&self, name: &'static str) -> BasicBlock<'ctx> {
-        match self.curr_fn_ret_bb {
-            Some(bb) => self.context.prepend_basic_block(bb, name),
-            None => self
-                .context
-                .append_basic_block(self.curr_func.unwrap(), name),
         }
     }
 
@@ -1238,28 +1216,6 @@ impl<'ctx> CodeGen<'ctx> {
         Ok(Some(load_val))
     }
 
-
-    fn okta_type_to_llvm(&self, var_type: &VarType) -> Box<BasicTypeEnum<'ctx>> {
-        Box::new(match var_type {
-            VarType::Int8 | VarType::UInt8 => self.context.i8_type().as_basic_type_enum(),
-            VarType::Int16 | VarType::UInt16 => self.context.i16_type().as_basic_type_enum(),
-            VarType::Int32 | VarType::UInt32 => self.context.i32_type().as_basic_type_enum(),
-            VarType::Int64 | VarType::UInt64 => self.context.i64_type().as_basic_type_enum(),
-            VarType::Float32 => self.context.f32_type().as_basic_type_enum(),
-            VarType::Float64 => self.context.f64_type().as_basic_type_enum(),
-            VarType::Boolean => self.context.bool_type().as_basic_type_enum(),
-            VarType::Array { inner, len } => self
-                .okta_type_to_llvm(inner)
-                .array_type(*len as u32)
-                .as_basic_type_enum(),
-            VarType::Unknown => unreachable!(),
-            VarType::Struct(name) => self.module.get_struct_type(name).unwrap().as_basic_type_enum(),
-            VarType::Ref(ty) => self.okta_type_to_llvm(ty)
-                .ptr_type(AddressSpace::Generic).as_basic_type_enum(),
-            VarType::Enum(name) => self.module.get_struct_type(name).unwrap().as_basic_type_enum(),
-            // _ => todo!(),
-        })
-    }
 
     /*
     /// Writes module bitcode to a file in the given path
---
+++ src/codegen/utils.rs
@@ -0,0 +1,57 @@
+use inkwell::basic_block::BasicBlock;
+use inkwell::AddressSpace;
+use inkwell::values::PointerValue;
+use inkwell::types::{BasicType, BasicTypeEnum};
+
+use crate::VarType;
+
+use super::CodeGen;
+
+impl<'ctx> CodeGen<'ctx> {
+
+    pub(super) fn create_entry_block_alloca<T: BasicType<'ctx>>(
+        &self,
+        name: &str,
+        var_type: T,
+    ) -> PointerValue<'ctx> {
+        let builder = self.context.create_builder();
+        let entry = self.curr_func.unwrap().get_first_basic_block().unwrap();
+        match entry.get_first_instruction() {
+            Some(first_instr) => builder.position_before(&first_instr),
+            None => builder.position_at_end(entry),
+        }
+
+        builder.build_alloca(var_type, name)
+    }
+
+    pub(super) fn create_basic_block(&self, name: &'static str) -> BasicBlock<'ctx> {
+        match self.curr_fn_ret_bb {
+            Some(bb) => self.context.prepend_basic_block(bb, name),
+            None => self
+                .context
+                .append_basic_block(self.curr_func.unwrap(), name),
+        }
+    }
+
+    pub(super) fn okta_type_to_llvm(&self, var_type: &VarType) -> Box<BasicTypeEnum<'ctx>> {
+        Box::new(match var_type {
+            VarType::Int8 | VarType::UInt8 => self.context.i8_type().as_basic_type_enum(),
+            VarType::Int16 | VarType::UInt16 => self.context.i16_type().as_basic_type_enum(),
+            VarType::Int32 | VarType::UInt32 => self.context.i32_type().as_basic_type_enum(),
+            VarType::Int64 | VarType::UInt64 => self.context.i64_type().as_basic_type_enum(),
+            VarType::Float32 => self.context.f32_type().as_basic_type_enum(),
+            VarType::Float64 => self.context.f64_type().as_basic_type_enum(),
+            VarType::Boolean => self.context.bool_type().as_basic_type_enum(),
+            VarType::Array { inner, len } => self
+                .okta_type_to_llvm(inner)
+                .array_type(*len as u32)
+                .as_basic_type_enum(),
+            VarType::Unknown => unreachable!(),
+            VarType::Struct(name) => self.module.get_struct_type(name).unwrap().as_basic_type_enum(),
+            VarType::Ref(ty) => self.okta_type_to_llvm(ty)
+                .ptr_type(AddressSpace::Generic).as_basic_type_enum(),
+            VarType::Enum(name) => self.module.get_struct_type(name).unwrap().as_basic_type_enum(),
+            // _ => todo!(),
+        })
+    }
+}
