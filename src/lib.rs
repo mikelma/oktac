f9547fa6ec2e4ec123837686a685d7631cd3fc1f
mikelma <mikelma7@gmail.com>
2021-10-28 15:46:46 +0200

    GlobStatus renamed to GlobalStatus and types module added

--- src/lib.rs
+++ src/lib.rs
@@ -1,121 +1,34 @@
 #[macro_use]
 extern crate pest_derive;
 
-// #[macro_use]
-// extern crate lazy_static;
-
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
+
+mod types;
 pub mod args;
 pub mod ast;
 pub mod codegen;
 pub mod msg;
 pub mod st;
 
+pub use types::VarType;
 pub use args::{EmitOpts, Opts};
 pub use ast::{AstNode, BinaryOp, UnaryOp, Visibility};
 pub use codegen::CodeGen;
 pub use msg::{LogMesg, MessageType};
 pub use st::ST;
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum VarType {
-    UInt8,
-    Int8,
-    UInt16,
-    Int16,
-    UInt32,
-    Int32,
-    Int64,
-    UInt64,
-    Float32,
-    Float64,
-    Boolean,
-    Array {
-        inner: Box<VarType>,
-        len: usize,
-    },
-    /// Contains the name of the struct type.
-    Struct(String),
-    /// Contains the name of the enum type.
-    Enum(String),
-    // Contains the type it refers to.
-    Ref(Box<VarType>),
-    Unknown,
-}
-
-pub struct GlobStatus {
+pub struct GlobalStatus {
     /// number of errors
     pub errors: usize,
     /// number of warnings
     pub warnings: usize,
 }
 
-pub static GLOBAL_STAT: Lazy<Mutex<GlobStatus>> = Lazy::new(|| {
-    Mutex::new(GlobStatus {
+pub static GLOBAL_STAT: Lazy<Mutex<GlobalStatus>> = Lazy::new(|| {
+    Mutex::new(GlobalStatus {
         errors: 0,
         warnings: 0,
     })
 });
-
-impl VarType {
-    /// Returns the size of the type in bytes. If the type is `Unknown`,
-    ///
-    /// **NOTE**: If the size in bits of the type is lower than 8, this function will return 1 as
-    /// the size in bytes of the type.
-    pub fn size(&self) -> usize {
-        match &self {
-            VarType::UInt8 | VarType::Int8 => 1,
-            VarType::UInt16 | VarType::Int16 => 2,
-            VarType::UInt32 | VarType::Int32 | VarType::Float32 => 4,
-            VarType::Int64 | VarType::UInt64 | VarType::Float64 => 8,
-            VarType::Boolean => 1,
-            VarType::Array { inner, .. } => inner.size(),
-            VarType::Ref(inner) => inner.size(),
-            VarType::Struct(name) => match ST.lock().unwrap().search_struct(name) {
-                Ok(Some(members)) => members.iter().map(|(_, ty)| ty.size()).sum(),
-                Ok(None) => 0,
-                Err(e) => {
-                    e.send().unwrap();
-                    0
-                }
-            },
-            VarType::Enum(name) => {
-                match ST.lock().unwrap().search_enum(name) {
-                    Ok(Some(fields)) => fields
-                        .iter()
-                        .map(|(_, fields)| fields.iter().map(|(_, ty)| ty.size()).sum())
-                        .max()
-                        .unwrap_or(0), // if there are no fields in any of the variants
-                    Ok(None) => 0,
-                    Err(e) => {
-                        e.send().unwrap();
-                        0
-                    }
-                }
-            }
-            // variants.iter()
-            // .map(|(_, fields)| fields.iter().map(|(_, ty)| ty.size()).sum()).max().unwrap_or(0),
-            VarType::Unknown => 0,
-        }
-    }
-
-    /*
-    /// Returns the "depth" of the array type. If `Self` is not of `Array` type, returns 0, else a
-    /// number >= 1.
-    pub fn array_depth(&self) -> usize {
-        let mut depth = 0;
-        let mut t = self;
-        loop {
-            match t {
-                VarType::Array { inner, .. } => {
-                    depth += 1;
-                    t = &**inner;
-                }
-                _ => return depth,
-            }
-        }
-    }
-    */
-}
