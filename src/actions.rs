213016fbe2c9922856b699637abe26165f7c7444
mikelma <mikelma7@gmail.com>
2021-10-30 17:02:09 +0200

    print_ast, codegen and llvm_to_bin functions added to actions module

--- src/actions.rs
+++ src/actions.rs
@@ -1,12 +1,21 @@
 use console::style;
+use inkwell::context::Context;
 
 use std::thread;
 use std::sync::{Mutex, Arc, Barrier};
-use std::fs::File;
+use std::fs::{File, self};
 use std::io::prelude::*;
 use std::process;
+use std::path::{PathBuf, Path};
+use std::hash::*;
+use std::collections::hash_map::DefaultHasher;
+use std::process::Command;
 
-use crate::{GLOBAL_STAT, CompUnitStatus, current_unit_status, ast};
+use crate::{
+    GLOBAL_STAT, CompUnitStatus, 
+    current_unit_status, ast,
+    CodeGen,
+};
 
 /// Reads all input files and generates the AST of compilation unit (that are stored in each 
 /// CompUnitStatus inside the GlobalStatus). All this process occurs in parallel, as each 
@@ -50,15 +59,23 @@ pub fn source_to_ast(paths: Vec<String>) {
                 }
             };
 
+            let mut hasher = DefaultHasher::new();
+
             let protos = ast::generate_protos(pairs.clone());
             
-            current_unit_status!().lock().unwrap().protos = protos;
+            protos.hash(&mut hasher);
+
+            current_unit_status!().lock().unwrap().protos = Arc::new(protos);
 
             protos_ready.wait();
 
             let ast = ast::generate_ast(pairs);
+            
+            ast.hash(&mut hasher);
 
-            current_unit_status!().lock().unwrap().ast = ast;
+            current_unit_status!().lock().unwrap().ast = Arc::new(ast);
+            // generate the hash of the compilation unit, based on it's AST (including protos)
+            current_unit_status!().lock().unwrap().hash = hasher.finish();
         }));
     }
 
@@ -117,4 +134,138 @@ pub fn show_astgen_msgs() -> Result<(), ()> {
         Ok(())
     }
 
+}
+
+pub fn print_ast() {
+    let multiple_units = GLOBAL_STAT.lock().unwrap().units.len() > 1;
+    for unit in GLOBAL_STAT.lock().unwrap().units.values() {
+
+        if multiple_units {
+            println!("\n> Compilation unit: {}", unit.lock().unwrap().filename);
+        }
+
+        println!("{:#?}", unit.lock().unwrap().protos);
+        println!("{:#?}", unit.lock().unwrap().ast);
+
+    }
+}
+
+/// Generates the LLVM-IR of the compilation units (in parallel) and saves them into a file 
+/// (one for each unit) in the temporal directory.
+/// If the temporal directory does not exist, the function will try to create it.
+pub fn codegen(tmp_dir: PathBuf) {
+    // ensure that the directory for temporal files exists before 
+    // the compilation step
+    if !tmp_dir.exists() {
+        if let Err(e) = fs::create_dir_all(&tmp_dir) {
+            eprintln!("[ERR] Cannot create temporal directory {}: {}", 
+                      tmp_dir.to_str().unwrap_or(""), e);
+            process::exit(1);
+        }
+    }
+
+    // old thread-ids of the compilation units (these will be replaced by codegen thread-ids)
+    let old_keys: Vec<thread::ThreadId> = GLOBAL_STAT.lock()
+        .unwrap()
+        .units
+        .keys()
+        .cloned()
+        .collect();
+
+    let mut thread_handles = vec![];
+    let compilation_err_mutex = Arc::new(Mutex::new(false));
+    let shared_tmp_dir = Arc::new(tmp_dir);
+
+    for old_key in old_keys {
+        let unit = GLOBAL_STAT.lock().unwrap().units.remove(&old_key).unwrap();
+        let compile_errors = Arc::clone(&compilation_err_mutex);
+        let filename = unit.lock().unwrap().filename.clone();
+        let tmp_dir_ref = Arc::clone(&shared_tmp_dir);
+        
+        thread_handles.push(thread::spawn(move || {
+
+            // insert the unit in the global status with the new codegen thread-id as it's key
+            GLOBAL_STAT.lock().unwrap().units.insert(thread::current().id(), unit);
+
+            // compile the AST to LLVM-IR
+            let context = Context::create();
+            let mut codegen = CodeGen::new(&context);
+
+            let protos = Arc::clone(&current_unit_status!().lock().unwrap().protos);
+            let ast = Arc::clone(&current_unit_status!().lock().unwrap().ast);
+
+            if let Err(e) = codegen.compile(&protos, &ast) {
+                // note that this scope will run as a critical section (thus, we can print the
+                // error safely here)
+                let mut has_err = compile_errors.lock().unwrap();
+                eprintln!("Inner error occurred in the codegen step for file {}: {}", &filename, e);
+                *has_err = true;
+            }
+
+            // save the generated IR to the temporal directory
+            let fname = tmp_dir_ref.join(format!("{:x}.ll", current_unit_status!().lock().unwrap().hash));
+            if let Err(e) = fs::write(&fname, codegen.to_string()) {
+                let mut has_err = compile_errors.lock().unwrap();
+                eprintln!("Inner error occurred while writing temporal file {}: {}", fname.to_str().unwrap(), e);
+                *has_err = true;
+            }
+        }));
+    }
+
+    for handle in thread_handles {
+        handle.join().unwrap();
+    }
+}
+
+/// Compiles the LLVM-IR of each unit, creating an executable binary
+/// The function will exit after an error message if the compilation fails. 
+pub fn llvm_to_bin(tmp_dir: PathBuf, output: &str, c_include: Option<&Vec<String>>) {
+    let mut to_compile = GLOBAL_STAT.lock()
+        .unwrap()
+        .units
+        .values()
+        .map(|u| tmp_dir.join(format!("{:x}.ll", u.lock().unwrap().hash)).to_str().unwrap().into())
+        .collect::<Vec<String>>();
+
+    let mut cmd = Command::new("clang");
+
+    cmd.arg("-O0");
+
+    // get the header and C files to compile (if some)
+    if let Some(files) = c_include {
+        for file in files {
+            if let Some(extension) = Path::new(&file).extension() {
+                if extension == "h" {
+                    cmd.args(&["-I", &file]);
+
+                } else {
+                    to_compile.push(file.into());
+                }
+            }
+        }
+    }
+
+    cmd.args(&to_compile);
+
+    cmd.arg(format!("-o{}", output));
+
+    match cmd.status() {
+        Ok(stat) => {
+            if !stat.success() {
+                eprintln!("Failed to run clang command:");
+                match stat.code() {
+                    Some(code) => eprintln!("\t* Clang exited with status code: {}", code),
+                    None => eprintln!("\t* Process terminated by signal"),
+                }
+                eprintln!("\t* Command:{:?}", cmd);
+                process::exit(1);
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to run clang command:");
+            eprintln!("\t* Error: {}", e);
+            eprintln!("\t* Command: {:?}", cmd);
+            process::exit(1);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use clap::Clap;
+use clap::Parser;
 use console::style;
-// use inkwell::context::Context;
 
-// use std::path::Path;
-// use std::process::{self, Command};
 use std::process;
+use std::path::PathBuf;
 
 use oktac::*;
 
@@ -18,97 +16,15 @@ fn main() {
         process::exit(1);
     }
 
-    /*
-    // check for number of semantic errors
-    let n_errs: usize = GLOBAL_STAT.units.values().map(|u| u.lock().unwrap().errors).sum();
-    let n_warns: usize = GLOBAL_STAT.units.values().map(|u| u.lock().unwrap().warnings).sum();
-
-    if n_warns > 0 {
-        eprintln!(
-            "{}",
-            style(format!("[W] {} warnings emitted\n", n_warns))
-                .bold()
-                .yellow()
-        );
-    }
-    if n_errs > 0 {
-        eprintln!("[!] Compilation failed due to {} errors", n_errs);
-        process::exit(1);
-    }
-
     if opts.emit_ast > 0 {
-        eprintln!("{:#?}", protos);
-        eprintln!("{:#?}", ast);
+        actions::print_ast();
     }
 
-    // compile the AST to LLVM-IR
-    let context = Context::create();
-    let mut codegen = CodeGen::new(&context);
-    let mut comp_errs = false;
+    let tmp_dir = PathBuf::from(&opts.tmp_dir);
 
-    if let Err(e) = codegen.compile(&protos, &ast) {
-        eprintln!("[ERR] Compilation error: {}", e);
-        comp_errs = true;
-    }
+    actions::codegen(tmp_dir.clone());
 
-    if comp_errs {
-        // exit if the compilation was not successful
-        eprintln!("Compilation failed due to inner compiler errors! =(");
-        process::exit(1);
-    }
-
-    if opts.emit_llvm > 0 {
-        println!("{}", codegen.to_string());
-    }
-
-    // write compiled bitcode to a temporary file
-    if let Err(e) = fs::write("tmp.ll", codegen.to_string())
-    // codegen.write_bc(&Path::new("tmp.bc")) {
-    {
-        eprintln!("{}", e);
-        process::exit(1);
-    }
-
-    // compile the bitcode into a binary using clang
-    let mut cmd = Command::new("clang");
-    cmd.arg("-O0");
-
-    let mut compile_files = vec!["tmp.ll".to_string()]; // files to compile
-
-    // get the header and C files to compile (if some)
-    if let Some(files) = opts.c_include {
-        for file in files {
-            if let Some(extension) = Path::new(&file).extension() {
-                if extension == "h" {
-                    cmd.args(&["-I", &file]);
-                } else {
-                    compile_files.push(file);
-                }
-            }
-        }
-    }
-
-    cmd.args(&compile_files);
-
-    cmd.arg(format!("-o{}", opts.output));
-    match cmd.status() {
-        Ok(stat) => {
-            if !stat.success() {
-                eprintln!("Failed to run clang command:");
-                match stat.code() {
-                    Some(code) => eprintln!("\t* Clang exited with status code: {}", code),
-                    None => eprintln!("\t* Process terminated by signal"),
-                }
-                eprintln!("\t* Command:{:?}", cmd);
-                process::exit(1);
-            }
-        }
-        Err(e) => {
-            eprintln!("Failed to run clang command:");
-            eprintln!("\t* Error: {}", e);
-            eprintln!("\t* Command: {:?}", cmd);
-            process::exit(1);
-        }
-    }
-    */
+    actions::llvm_to_bin(tmp_dir, 
+                         &opts.output, 
+                         opts.c_include.as_ref())
 }
