26e6717ddce487beafbf2b0a5ec5aa18bbf7bf0f
mikelma <mikelma7@gmail.com>
2022-02-24 11:44:21 +0000

    macro examples updated

---
+++ examples/macros/derives/derive_pizza.lua
@@ -0,0 +1,26 @@
+-- all generated AST nodes will be appended here
+result = {} 
+
+-- a copy of the input struct but with a different name 
+pizza_struct = okta.macro.input[1]
+pizza_struct.StructProto.name = "Pizza"
+
+-- add the newly created struct to the result
+result[1] = pizza_struct
+
+-- we have to register the struct `Pizza` in order to use it inside
+-- the `quote` function in the lines below
+okta.register(pizza_struct)
+
+-- the AST of a function is a list of two AST nodes, the function's
+-- prototype (similar to a header) and the function declaration (includes
+-- function's body)
+new_func = okta.quote([[
+pub fun print_pizza(pizza: &Pizza) {
+    printf(@cstr("Pizza { x: %d, y: %f }\n"), *pizza.x, *pizza.y);
+}]])
+
+result[2] = new_func[1] -- append prototype
+result[3] = new_func[2] -- append declaration
+
+return result
--- examples/macros/simple/println.lua
+++ examples/macros/simple/println.lua
@@ -1,10 +1,10 @@
+-- This function is used to determine if a table `tab` contains the value `val` inside.
 local function has_value (tab, val)
     for index, value in ipairs(tab) do
         if value == val then
             return true
         end
     end
-
     return false
 end
 
@@ -12,25 +12,32 @@ local int_types = {"i8", "u8", "i16", "u16", "i32", "u32", "i64", "u64"};
 local float_types = {"f32", "f64"};
 local string_type = "str";
 
-local fmt_str = "";
-local vals_str = "";
+local fmt_str = "";  -- format string to pass to the `printf` function
 
-for i = 1, (#okta.macro.input) do
+for i = 1, (#okta.macro.input) do -- for each input node
+    -- obtain the i-th node of the AST
     local node = okta.macro.input[i]
+    -- get the type of the node
     local node_ty = okta.node_type(node)
 
+    --get separator
     local pre  = (i==1 and "" or "\t")
 
+    -- if the node is a integer type
     if has_value(int_types, node_ty) then
         fmt_str = fmt_str..pre.."%d"
 
+    -- if the node is a float type
     elseif has_value(float_types, node_ty) then
         fmt_str = fmt_str..pre.."%f"
 
+    -- if the node is a string type
     elseif node_ty == string_type then
         fmt_str = fmt_str..pre.."%s"
 
+    -- the node cannot be formatted using basic formatting
     else
+        -- return a compilation error from inside the macro
         okta.compiler_error({
             cause = "Cannot format type "..node_ty.." with the default formatter"
         })
@@ -41,9 +48,13 @@ fmt_str = fmt_str .. "\n"
 
 local print_call = okta.quote("printf(@cstr(\""..fmt_str.."\"));")
 
+local vals_str = ""; -- string containing the values to print
+
+-- append parameters to the `printf` call
 for i = 1, (#okta.macro.input) do
     local node = okta.macro.input[i]
 
+    -- if the node is of string type, it has to be converted to a C string
     if okta.node_type(node) == "str" then
         local cstr = okta.quote("@cstr(\"\");")[1]
         cstr.FunCall.params[1] = node
